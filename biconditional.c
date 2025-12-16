096bc224e2da9ea16f4f6cceb70859c74dd29f71
Sabal Sigdel <sigdelsabal20@gmail.com>
2025-12-16 18:45:13 +0545

    #1

--- biconditional.c
+++ biconditional.c
@@ -1,28 +1,25 @@
-#include<stdio.h> //1
+#include<stdio.h>
+#include <conio.h>
 
 int biconditional (int a, int b){
     if(a==b)
-        return 1;
-    
+        return 1;    
     else 
-
     return 0;
 }
 int main(){
+    printf("Biconditional truth table\n\n");
     printf("A\tB\tA<-->B\n");
-    int a = 1, b = 1, count =1;
-    while (count<5){
-        
-        
-        if(count%2==1){
-            a = (a==1)? 0 : 1;
+
+    for(int a = 0; a <=1; a++){
+        for (int b = 0 ; b <=1 ; b++){
+            printf("%d\t%d\t   %d\n",a,b,biconditional(a,b));
         }
-        b = (b==1) ? 0 : 1;
-
-        printf("%d\t%d\t%d\n",a,b,biconditional(a,b));
-        count++;
-    }
+    }      
     
-
+    
+printf("\n\nPrinted by Sabal Sigdel");
+getch();
     return 0;
 }
+
